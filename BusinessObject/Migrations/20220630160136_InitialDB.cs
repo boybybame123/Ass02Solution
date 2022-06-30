7abaa187d023252b9ff9773cd39a89cc3e737a04
boybybame123 <boybybame@gmail.com>
2022-06-30 23:39:20 +0700

    Add project files.

---
+++ BusinessObject/Migrations/20220630160136_InitialDB.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BusinessObject.Migrations
+{
+    public partial class InitialDB : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Member",
+                columns: table => new
+                {
+                    MemberId = table.Column<int>(type: "int", nullable: false),
+                    Email = table.Column<string>(type: "varchar(100)", unicode: false, maxLength: 100, nullable: false),
+                    CompanyName = table.Column<string>(type: "varchar(40)", unicode: false, maxLength: 40, nullable: false),
+                    City = table.Column<string>(type: "varchar(15)", unicode: false, maxLength: 15, nullable: false),
+                    Country = table.Column<string>(type: "varchar(15)", unicode: false, maxLength: 15, nullable: false),
+                    Password = table.Column<string>(type: "varchar(30)", unicode: false, maxLength: 30, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Member", x => x.MemberId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Product",
+                columns: table => new
+                {
+                    ProductId = table.Column<int>(type: "int", nullable: false),
+                    CategoryId = table.Column<int>(type: "int", nullable: false),
+                    ProductName = table.Column<string>(type: "varchar(40)", unicode: false, maxLength: 40, nullable: false),
+                    Weight = table.Column<string>(type: "varchar(20)", unicode: false, maxLength: 20, nullable: false),
+                    UnitPrice = table.Column<decimal>(type: "money", nullable: false),
+                    UnitslnStock = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Product", x => x.ProductId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Order",
+                columns: table => new
+                {
+                    OrderId = table.Column<int>(type: "int", nullable: false),
+                    MemberId = table.Column<int>(type: "int", nullable: false),
+                    OrderDate = table.Column<DateTime>(type: "datetime", nullable: false),
+                    RequiredDate = table.Column<DateTime>(type: "datetime", nullable: true),
+                    ShippedDate = table.Column<DateTime>(type: "datetime", nullable: true),
+                    Freight = table.Column<decimal>(type: "money", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Order", x => x.OrderId);
+                    table.ForeignKey(
+                        name: "FK_Order_Member",
+                        column: x => x.OrderId,
+                        principalTable: "Member",
+                        principalColumn: "MemberId");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "OrderDetail",
+                columns: table => new
+                {
+                    OrderId = table.Column<int>(type: "int", nullable: false),
+                    ProductId = table.Column<int>(type: "int", nullable: false),
+                    UnitPrice = table.Column<decimal>(type: "money", nullable: false),
+                    Quantity = table.Column<int>(type: "int", nullable: false),
+                    Discount = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OrderDetail", x => x.OrderId);
+                    table.ForeignKey(
+                        name: "FK_OrderDetail_Order",
+                        column: x => x.OrderId,
+                        principalTable: "Order",
+                        principalColumn: "OrderId");
+                    table.ForeignKey(
+                        name: "FK_OrderDetail_Product",
+                        column: x => x.ProductId,
+                        principalTable: "Product",
+                        principalColumn: "ProductId");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderDetail_ProductId",
+                table: "OrderDetail",
+                column: "ProductId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "OrderDetail");
+
+            migrationBuilder.DropTable(
+                name: "Order");
+
+            migrationBuilder.DropTable(
+                name: "Product");
+
+            migrationBuilder.DropTable(
+                name: "Member");
+        }
+    }
+}
---
+++ BusinessObject/Models/Member.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BusinessObject.Models
+{
+    public partial class Member
+    {
+        public int MemberId { get; set; }
+        public string Email { get; set; } = null!;
+        public string CompanyName { get; set; } = null!;
+        public string City { get; set; } = null!;
+        public string Country { get; set; } = null!;
+        public string Password { get; set; } = null!;
+
+        public virtual Order Order { get; set; } = null!;
+    }
+}
